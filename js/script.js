6f876bf05023fee49c00dfd232e71495b2c03744
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2023-11-19 16:58:56 -0500

    Add form structure html, styles, and functionalities

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// Selecting form and input password element
+const form = document.querySelector('.form');
+const passwordInput = document.querySelector('.form_input-pass');
+const passToggleBtn = document.querySelector('#pass-toggle-btn');
+
+// Function to display error messages
+const showError = (field, errorText) => {
+    field.classList.add('error');
+    const errorElement = document.createElement('small');
+    errorElement.classList.add('error-text');
+    errorElement.innerText = errorText;
+    field.closest('.form_group').appendChild(errorElement);
+}
+
+// Function to handle form submission
+const handleFormData = (e) => {
+    e.preventDefault();
+
+    // Retrieving input elements
+    const fullNameInput = document.querySelector('#fullname');
+    const emailInput = document.querySelector('#email');
+    const dateInput = document.querySelector('#date');
+    const genderInput = document.querySelector('#gender');
+
+    // Getting trimmed values from input fields
+    const fullName = fullNameInput.value.trim();
+    // console.log(fullName);
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+    const date = dateInput.value;
+    const gender = genderInput.value;
+
+    // Regular expression pattern for email validation
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    
+    // Clearing previous error messages
+    document.querySelectorAll('.form_group .error').forEach(field => field.classList.remove('error'));
+    document.querySelectorAll('.error-text').forEach(errorText => errorText.remove());
+
+    // Performing validation checks
+    if (fullName === '') {
+        showError(fullNameInput, 'Enter your full name');
+    }
+    if (!emailPattern.test(email)) {
+        showError(emailInput, 'Enter a valid email address');
+    }
+    if (password === '') {
+        showError(passwordInput, 'Enter your password');
+    }
+    if (date === '') {
+        showError(dateInput, 'Select your date of birth');
+    }
+    if (gender === '') {
+        showError(genderInput, 'Select your gender');
+    }
+
+    // Checking for any remaining errors before form submission
+    const errorInputs = document.querySelectorAll('.form_group .error');
+    if (errorInputs.length > 0) return;
+
+    // Submitting the form
+    form.submit();
+}
+
+// Toggling password visibility
+passToggleBtn.addEventListener('click', () => {
+    passToggleBtn.className = passwordInput.type === 'password' ? 'fa-solid fa-eye-slash' : 'fa-solid fa-eye';
+    passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+})
+
+// Handling form submission event
+form.addEventListener('submit', handleFormData);
